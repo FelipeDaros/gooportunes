5234ae5fdb300130c984a11d817ef1f824e8518c
FelipeDaros <felipe-daros@hotmail.com>
2024-01-20 12:47:09 -0300

    feat: add debug

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/FelipeDaros/gooportunes/router"
+import (
+	"github.com/FelipeDaros/gooportunes/config"
+	"github.com/FelipeDaros/gooportunes/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config", err)
+		return
+	}
+
 	router.Initialize()
 }
